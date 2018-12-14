583f19f98d055b10f0655f440195916ed4bd0caf
Scotty Goldsmith <scottygoldsmith@Scottys-MBP-2.(none)>
2018-12-14 04:00:37 -0700

    Initial Commit

---
+++ Tadoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tadoey
+//
+//  Created by Scotty Goldsmith on 12/14/18.
+//  Copyright © 2018 Scotty Goldsmith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
