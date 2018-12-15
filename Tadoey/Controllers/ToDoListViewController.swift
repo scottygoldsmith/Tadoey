b27333a686319f12b66ecca2163b7d48637933bc
Scotty Goldsmith <scottygoldsmith@Scottys-MBP-2.(none)>
2018-12-15 07:15:52 -0700

    Persisting data using Codable protocol and files

--- Tadoey/Controllers/ToDoListViewController.swift
+++ Tadoey/Controllers/ToDoListViewController.swift
@@ -11,28 +11,14 @@ import UIKit
 class ToDoListViewController: UITableViewController {
     
     var itemArray = [Items]()
-    let defaults = UserDefaults.standard
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let newItem = Items()
-        newItem.title = "Find Mike"
-        itemArray.append(newItem)
-        
-        let newItem2 = Items()
-        newItem2.title = "Buy Eggos"
-        itemArray.append(newItem2)
-
-        let newItem3 = Items()
-        newItem3.title = "Destroy Demogorgon"
-        itemArray.append(newItem3)
-        
-        // Do any additional setup after loading the view, typically from a nib.
-        //Load data from UserDefaults
-        if let items = defaults.array(forKey: "TodoListArray") as? [Items] {
-            itemArray = items
-        }
+        //Load Data
+        loadItems()
     }
 
     //MARK - Tableview Datasource Methods
@@ -61,9 +47,8 @@ class ToDoListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         //Add or Remove Checkmark
         itemArray[indexPath.row].done = !itemArray[indexPath.row].done //if true, make false.  If false, make true.
-        tableView.reloadData()
+        saveItems()
         tableView.deselectRow(at: indexPath, animated: true)
-        
     }
     
     //Mark - Add New Items
@@ -76,10 +61,10 @@ class ToDoListViewController: UITableViewController {
             let newItem = Items()
             newItem.title = textField.text!
             self.itemArray.append(newItem)
-            //Save data to UserDefaults
-            self.defaults.set(self.itemArray, forKey: "TodoListArray")
-            self.tableView.reloadData()
+            self.saveItems()
         }
+        
+    
         
         //Add Text Field Input to Alert Window
         alert.addTextField { (alertTextField) in
@@ -87,12 +72,41 @@ class ToDoListViewController: UITableViewController {
             textField = alertTextField
         }
         
-        
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
     }
     
+    //MARK - Model Manipulation Methods
     
+    func saveItems() {
+        // ADD "CODABLE" TO DATA MODEL!!
+        //Encode and save data
+        let encoder = PropertyListEncoder()
+        do {
+            let data = try encoder.encode(itemArray)
+            try data.write(to: dataFilePath!)
+        } catch {
+            print("Error encoding item array, \(error)")
+        }
+        
+        tableView.reloadData()
+    }
+    
+    func loadItems() {
+        
+        //Decode and load
+        if let data = try? Data(contentsOf: dataFilePath!) {
+           let decoder = PropertyListDecoder()
+            do {
+            itemArray = try decoder.decode([Items].self, from: data)
+            } catch {
+                print("Error decoding item array, \(error)")
+            }
+            
+        }
+        
+    }
     
 }
+
 
